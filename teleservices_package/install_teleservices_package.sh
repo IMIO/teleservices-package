3f49ee077f1c91be0baee5ee29c18debbda83693
Nicolas Selva <nicolas.selva@imio.be>
2023-05-10 14:01:17 +0200

    [TELE-1682]add casier judiciaire passerelle

--- teleservices_package/install_teleservices_package.sh
+++ teleservices_package/install_teleservices_package.sh
@@ -5,4 +5,11 @@ set -e # Exit immediately if a command exits with a non-zero status.
 # installation path
 install_path="/usr/lib/teleservices_package"
 
+echo "--- Install passerelle settings"
+if [ ! -f "/etc/passerelle/settings.d/casier_judiciaire.py" ]
+then
+  cp $install_path/passerelle/casier_judiciaire.py /etc/passerelle/settings.d/
+  service passerelle restart
+fi
+
 sudo -u hobo hobo-manage imio_indus_deploy --directory $install_path
